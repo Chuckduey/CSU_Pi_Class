f25a531e2877e8c9f85fd0e2226c1fccf0c224be
Chuck Duey <chuck@frankenchiller.com>
2019-03-26 18:51:20 -0600

    Adding session cookies and logins

--- hello_web.py
+++ hello_web.py
@@ -1,11 +1,12 @@
 import socket
 import sys
 # Define socket to be used for web page  80 is a normal web socket, but may be used for other things in the system.
-HOST, PORT = '', 80
+HOST, PORT = '', 1080
 
 # Define the standard web response for a web page
 web_response = """\
 HTTP/1.1 200 OK
+Set-Cookie: session=12345
 
 """
 web_line = '<html><h1>Hello World</h1></html>\n'
@@ -15,11 +16,15 @@ listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print( 'Serving HTTP on port %s ...' % PORT)
+print(web_response)
 while True:
 	# open socket
     client_connection, client_address = listen_socket.accept()
 # wait for response
-    request = client_connection.recv(1024)
+    try:
+        request = client_connection.recv(1024)
+    except:
+         print("Web Exception")
     print(request)
     try:
        client_connection.sendall(web_response.encode('utf8'))
