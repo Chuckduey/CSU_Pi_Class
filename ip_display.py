b945acef9a4d15e30ae3e767d16c3f13f15241be
Chuckduey <cduey@msn.com>
2020-02-09 11:49:52 -0700

    updated ip dosplay so it continues to loop if there is no network

--- ip_display.py
+++ ip_display.py
@@ -7,18 +7,23 @@ from Tkinter import *
 host = "None"
 ipaddr = "None"
 gateway = "None"
-update = 3000
+update = 5000
 
 #
 def update_ip():
      global clock
-     gw = os.popen("ip -4 route show default").read().split()
-     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-     s.connect((gw[2], 0))
-     ipaddr = s.getsockname()[0]
-     gateway = gw[2]
-     host = socket.gethostname()
-     s.close()
+     try:
+         gw = os.popen("ip -4 route show default").read().split()
+         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+         s.connect((gw[2], 0))
+         ipaddr = s.getsockname()[0]
+         gateway = gw[2]
+         host = socket.gethostname()
+         s.close()
+     except:
+         gateway = "None"
+         host =  "None"
+         ipaddr = "None"
      clock.config(text="Host name = "+host+"\nIp Address = "+ ipaddr + "\nGateway =" + gateway, anchor="w")
      clock.after(update,update_ip)
      
