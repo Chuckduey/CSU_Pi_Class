53ebf5b53d252576a6234b1751c85bc08bcb10b2
chuckduey <cduey@msn.com>
2021-02-14 15:01:19 -0700

    Changing mraa to gpio control

--- html/button.php
+++ html/button.php
@@ -19,27 +19,27 @@ echo "State is = ",$state;
 </header>
         Control:
         <?php
-        $setmode4 = exec("/usr/bin/mraa-gpio set 7 output");
-        $setmode22 = exec("/usr/bin/mraa-gpio set 22 output");
-        # $setmode4 = shell_exec("/usr/bin/gpio -g mode 4 out");
-        # $setmode26 = shell_exec("/usr/bin/gpio -g mode 22 out");
+        # $setmode4 = exec("/usr/bin/mraa-gpio set 7 output");
+        # $setmode22 = exec("/usr/bin/mraa-gpio set 22 output");
+         $setmode4 = shell_exec("/usr/bin/gpio -g mode 4 out");
+         $setmode26 = shell_exec("/usr/bin/gpio -g mode 22 out");
         if($state & 1){
-                $gpio_on1 = exec("/usr/bin/mraa-gpio set 7 0");
+                $gpio_on1 = exec("/usr/bin/gpio write 4 0");
                 $but1_color="green";
                 echo " Relay 1 is on , ";
         }
         else{
-                $gpio_off1 = exec("/usr/bin/mraa-gpio set 7 1");
+                $gpio_off1 = exec("/usr/bin/gpio write 4 1");
                 $but1_color="red";
                 echo " Relay 1 is off , ";
         }
         if($state & 2){
-                $gpio_on2 = exec("/usr/bin/mraa-gpio set 15 0");
+                $gpio_on2 = exec("/usr/bin/gpio write 22 0");
                 echo " LED 2 is on ";
                 $but2_color="green";
         }
         else{
-                $gpio_off2 = exec("/usr/bin/mraa-gpio set 15 1");
+                $gpio_off2 = exec("/usr/bin/gpio write 22 1");
                 echo " LED 2 is off ";
                 $but2_color="red";
         }
